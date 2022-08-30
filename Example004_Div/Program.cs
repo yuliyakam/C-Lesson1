49ab754070b2e5ab793280631d3622484556929a
Yuliya <yulyakam85@gmail.com>
2022-08-30 19:28:05 +0400

    Add Random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1, 10); //Здесь будет случайное число от 1 до 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA/numberB);
